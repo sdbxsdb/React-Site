761cd950ab6940180c28fae253cb6ae8f52fa09e
Sam Donaghy-Bell <sam@reflex-studios.com>
2021-10-22 09:41:12 +0100

    routing down - adding nav bar next

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavMeetupsPage from './pages/FavMeetups';
 
 function App() {
   return (
 
     <div>
-      hello
+      <Switch>
+        <Route path="/" exact >
+          <AllMeetupsPage />
+        </Route>
+  
+        <Route path="/new-meetup" >
+          <NewMeetupPage />
+        </Route>
+  
+        <Route path="/fav-meetups" >
+          <FavMeetupsPage />
+        </Route>
+      </Switch>
 
     </div>
 
---
+++ src/pages/FavMeetups.js
@@ -0,0 +1,5 @@
+function FavMeetupsPage() {
+  return <div>Fav Meetups Page</div>;
+}
+
+export default FavMeetupsPage;
