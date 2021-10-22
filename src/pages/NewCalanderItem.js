dd88f2415b0fa44d165ae3b2f24d1d39dd26ad18
Sam Donaghy-Bell <sam@reflex-studios.com>
2021-10-22 15:18:17 +0100

    form submitting to DB

--- src/pages/NewCalanderItem.js
+++ src/pages/NewCalanderItem.js
@@ -1,8 +1,23 @@
+import { useHistory } from 'react-router-dom'
+
 import NewCalanderItemForm from '../components/calander/NewCalanderItemForm'
 
 function NewCalanderItem () {
+  const history = useHistory();
+  
   function addCalanderItemHandler(itemData) {
-    
+    fetch(
+      'https://react-test-62f63-default-rtdb.firebaseio.com/calanderItems.json', 
+      {
+        method: 'POST',
+        body: JSON.stringify(itemData),
+        headers: {
+          'content-type': 'application/json'
+        }
+      }
+    ).then(() => {
+      history.replace('/')
+    });
   }
   return (
     <section>
