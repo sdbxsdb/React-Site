48a4b40a18efc9ee355e2081da04c691fdfd513e
Sam Donaghy-Bell <sam@reflex-studios.com>
2021-10-25 09:40:41 +0100

    added total done badge

--- src/components/layout/MainNav.js
+++ src/components/layout/MainNav.js
@@ -1,9 +1,14 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom';
 
 import classes from './MainNav.module.css';
+import DoneContext from '../../store/done-context'
 
 
 function MainNav() {
+
+  const doneCtx = useContext(DoneContext);
+
   return (
   <header className={classes.header}>
     <div className={classes.logo}>My Calendar</div>
@@ -13,7 +18,10 @@ function MainNav() {
           <Link to='/'>All</Link>
         </li>
         <li>
-          <Link to='/done'>Done</Link>
+          <Link to='/done'>
+            Done
+            <small className={classes.badge}>( {doneCtx.totalDone} )</small>
+          </Link>
         </li>
         <li>
           <Link to='/new-item'>New</Link>
